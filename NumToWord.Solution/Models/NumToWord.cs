fb703286e8945e725a14aa41431f6ece1d9fd5b6
brevin <cronkbrevin98@gmail.com>
2020-05-14 15:39:49 -0700

    Testing done UI Build

--- NumToWord.Solution/Models/NumToWord.cs
+++ NumToWord.Solution/Models/NumToWord.cs
@@ -1,18 +1,74 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 
 namespace NumToWord
 {
     public class NumToWord
     {
         public static string Word {get; set;}
-        Dictionary<int, string> onesValues = new Dicitionary<int, string>();
-        Dictionary<int, string> tensValues = new Dicitionary<int, string>();
+        static Dictionary<string, string> onesValues = new Dictionary<string, string>();
+        static Dictionary<string, string> tensValues = new Dictionary<string, string>();
+
         public static void FillDictionaries()
         {
-            onesValues.Add({0, "Zero"}, {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}, {10, "Ten"}, {11, "Eleven"}, {12, "Twelve"}, {13, "Thirteen"}, {14, "Fourteen"},{15, "Fifteen"}, {16, "sixteen"}, {17, "Seventeen"}, {18, "Eighteen"}, {19, "Nineteen"});
-            tensValues.Add({0, "Zero"}, {1, "Ten"}, {2, "Twenty"}, {3, "Thirty"}, {4, "Forty"}, {5,"Fifty"},{6,"Sixty"}. {7,"Seventy"}. {8, "Eighty"}, {9, "Ninety"});
+            onesValues = new Dictionary<string, string>(){{"0", ""}, {"1", "One "}, {"2", "Two "}, {"3", "Three "}, {"4", "Four "}, {"5", "Five "}, {"6", "Six "}, {"7", "Seven "}, {"8", "Eight "}, {"9", "Nine "}, {"10", "Ten "}, {"11", "Eleven "}, {"12", "Twelve "}, {"13", "Thirteen "}, {"14", "Fourteen "},{"15", "Fifteen "}, {"16", "Sixteen "}, {"17", "Seventeen "}, {"18", "Eighteen "}, {"19", "Nineteen "}};
+            tensValues = new Dictionary<string, string>(){{"0", ""}, {"1", "Ten"}, {"2", "Twenty "}, {"3", "Thirty "}, {"4", "Forty "}, {"5","Fifty "},{"6","Sixty "}, {"7","Seventy "}, {"8", "Eighty "}, {"9", "Ninety "}};
         }
+        public static string NumberToWord(int number)
+        {
+            if (number == 0)
+            {
+                Word = "Zero";
+            }
+            else if(number > 0 && number <20)
+            {
+              Word = onesValues[number.ToString()];
+            }
+            else if (number < 0)
+            {
+                Word = "Negative " + NumberToWord(Math.Abs(number));
+            }
 
-    }
-
-}
+            else if (number > 0 && number >= 20)
+            {   
+                StringBuilder tempWords = new StringBuilder();
+                string numString = number.ToString();
+                string newString = String.Concat(numString.Reverse());
+                for (int i=0; i < numString.Length; i++)
+                {   decimal iCalc = (i/3);
+                    decimal numFamily = Math.Floor(iCalc);
+                        if (i%3 == 0 && numFamily == 0) // i = 0, 3, 6, 9
+                            {
+                                tempWords.Insert(0, onesValues[(newString[i].ToString())]);
+                            }
+                        else if (i%3 == 0 && numFamily == 1)
+                            {
+                            tempWords.Insert(0, onesValues[(newString[i].ToString())] + "Thousand ");
+                            }
+                        else if (i%3 == 0 && numFamily == 2)
+                        {
+                            tempWords.Insert(0, onesValues[(newString[i].ToString())] + "Million ");
+                        }
+                        else if (i%3 == 0 && numFamily == 3)
+                        {
+                            tempWords.Insert(0, onesValues[(newString[i].ToString())] + "Billion ");
+                        }
+                        else if (i%3 == 1) // i = 1, 4, 7, 10
+                            {
+                                tempWords.Insert(0, tensValues[(newString[i].ToString())]);
+                            }
+                        else if (i%3 == 2 && int.Parse((newString[i]).ToString()) != 0) //  i = 2, 5, 8, 11
+                            {
+                                tempWords.Insert(0, onesValues[(newString[i].ToString())]+ "Hundred ");
+                            }
+                    }
+                
+                Word = tempWords.ToString();
+                }
+                return Word;
+            }
+      }
+  }
--- NumToWord.Tests/Model.Tests/NumToWordTests.cs
+++ NumToWord.Tests/Model.Tests/NumToWordTests.cs
@@ -1,7 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using NumToWord;
+using System.Text;
 
 namespace NumToWord.Tests
 {
@@ -17,7 +19,64 @@ namespace NumToWord.Tests
       NumToWord.FillDictionaries();
       NumToWord.NumberToWord(19);
       // Assert
-      Assert.AreEqual("Nineteen", NumToWord.Word);
+      Assert.AreEqual("Nineteen ", NumToWord.Word);
     }
+    [TestMethod]
+    public void NumberToWord_CheckIfNegativeNumbersAboveNegative20ParseProperWords_NegativeNineteen()
+    {
+      NumToWord numbah = new NumToWord();
+      NumToWord.FillDictionaries();
+      NumToWord.NumberToWord(-19);
+      Assert.AreEqual("Negative Nineteen ", NumToWord.Word);
+    }
+    [TestMethod]
+    public void NumberToWord_DoSingleDigitNumbersConvertCorrectly_Nine()
+    {
+      NumToWord numbah = new NumToWord();
+      NumToWord.FillDictionaries();
+      NumToWord.NumberToWord(9);
+      Assert.AreEqual("Nine ", NumToWord.Word);
+    }
+    [TestMethod]
+    public void NumberToWord_DoDoubleDigitNumbersGreaterThan19ConvertCorrectly_ThirtyFive()
+    {
+      NumToWord numbah = new NumToWord();
+      NumToWord.FillDictionaries();
+      NumToWord.NumberToWord(35);
+      Assert.AreEqual("Thirty Five ", NumToWord.Word);
+    }
+    [TestMethod]
+    public void NumberToWord_DoThreeDigitNumbersConvertCorrectly_FourHundredThirtyFive()
+    {
+      NumToWord numbah = new NumToWord();
+      NumToWord.FillDictionaries();
+      NumToWord.NumberToWord(435);
+      Assert.AreEqual("Four Hundred Thirty Five ", NumToWord.Word);
+    }
+    [TestMethod]
+    public void NumberToWord_Do4DigitNumbersConvertCorrectly_FourThousandFourHundredThirtyGive()
+    {
+      NumToWord numbah = new NumToWord();
+      NumToWord.FillDictionaries();
+      NumToWord.NumberToWord(4435);
+      Assert.AreEqual("Four Thousand Four Hundred Thirty Five ", NumToWord.Word);
+    }
+    [TestMethod]
+    public void NumberToWord_DoMillionsNumbersConvertCorrectly_TwoMillionOneHundredFourThousandFourHundredThirtyFive()
+    {
+      NumToWord numbah = new NumToWord();
+      NumToWord.FillDictionaries();
+      NumToWord.NumberToWord(2104435);
+      Assert.AreEqual("Two Million One Hundred Four Thousand Four Hundred Thirty Five ", NumToWord.Word);
+    }
+    [TestMethod]
+    public void NumberToWord_DoBillionsNumbersConvertCorrectly_OneBillionTwoMillionOneHundredFourThousandFourHundredThirtyFive()
+    {
+      NumToWord numbah = new NumToWord();
+      NumToWord.FillDictionaries();
+      NumToWord.NumberToWord(1002104435);
+      Assert.AreEqual("One Billion Two Million One Hundred Four Thousand Four Hundred Thirty Five ", NumToWord.Word);
+    }
+
   }
 }
